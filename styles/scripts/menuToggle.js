8923e4a52b247024ea7a07fd8b3e660f212780a6
anthonyygpz@gmail.com <anthonyygpz@gmail.com>
2025-05-28 14:59:53 -0700

    feat: add remove show and navigate section

--- styles/scripts/menuToggle.js
+++ styles/scripts/menuToggle.js
@@ -4,7 +4,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   menuToggle.addEventListener("click", function () {
     navMenu.classList.toggle("show");
-
     // Cambiar el texto del botón
     if (navMenu.classList.contains("show")) {
       menuToggle.classList.add("fa-xmark");
@@ -21,7 +20,20 @@ document.addEventListener("DOMContentLoaded", function () {
     link.addEventListener("click", function (e) {
       if (this.getAttribute("href").startsWith("#")) {
         e.preventDefault();
+
+        // Quitar el menú
         navMenu.classList.remove("show");
+
+        // Restaurar el ícono a hamburguesa
+        menuToggle.classList.add("fa-bars");
+        menuToggle.classList.remove("fa-xmark");
+
+        // Opcional: Navegar a la sección después de cerrar el menú
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: "smooth" });
+        }
       }
     });
   });
